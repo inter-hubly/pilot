1c71526083099569b9dd2e1eb2625f8d966894f3
Saimon Ribeiro Silva <saimonribeiros@hotmail.com>
2024-11-18 17:37:20 -0300

    feat: init

--- server/envConfig.go
+++ server/envConfig.go
@@ -4,6 +4,8 @@ type dbConfig struct {
 	Host       string `yaml:"host"`
 	Database   string `yaml:"database"`
 	EntryPoint string `yaml:"entryPoint"`
+	Username   string `yaml:"username"`
+	Password   string `yaml:"password"`
 }
 
 type ampqConfig struct {
