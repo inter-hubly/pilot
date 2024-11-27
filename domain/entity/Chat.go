26bfac6c2e233d1f8790e6f593f521003b72893e
Saimon Ribeiro Silva <saimonribeiros@hotmail.com>
2024-11-27 16:41:41 -0300

    feat: init

--- domain/entity/Chat.go
+++ domain/entity/Chat.go
@@ -1,24 +1,19 @@
 package entity
 
-type ChatType string
-
-const WhatsApp ChatType = "WhatsApp"
-
 type Chat struct {
-	Id        string          `json:"id"`
-	MessageId string          `json:"messageId"`
-	Type      ChatType        `json:"type"`
-	Status    ChatMessageTime `json:"received,omitempty"`
-	Read      ChatMessageTime `json:"read,omitempty"`
-	Delivered ChatMessageTime `json:"delivered,omitempty"`
-	Message   ReceivedMessage `json:"message,omitempty"`
+	MessageId string                  `json:"messageId"`
+	OwnerId   string                  `json:"ownerId"`
+	From      string                  `json:"from"`
+	To        string                  `json:"to"`
+	Audit     []ChatMessageStatusTime `json:"status,omitempty"`
+	Message   ReceivedMessage         `json:"message,omitempty"`
 }
 
 type ReceivedMessage interface {
 	GetBody() string
 }
 
-type ChatMessageTime struct {
-	Status     string `json:"status"`
-	ReceivedAt string `json:"receivedAt"`
+type ChatMessageStatusTime struct {
+	Status     MessageStatus `json:"status"`
+	ReceivedAt string        `json:"receivedAt"`
 }
--- domain/entity/WhatsApp.go
+++ domain/entity/WhatsApp.go
@@ -1,28 +1,27 @@
 package entity
 
-type whatsAppMessage struct {
-	Body string `json:"body"`
-}
+//type whatsAppMessage struct {
+//	Body string `json:"body"`
+//}
+//
+//func NewWhatsAppMessage(body string) *whatsAppMessage {
+//	return &whatsAppMessage{
+//		Body: body,
+//	}
+//
+//}
+//
+//func (w *whatsAppMessage) GetBody() string {
+//	return w.Body
+//}
 
-func NewWhatsAppMessage(body string) *whatsAppMessage {
-	return &whatsAppMessage{
-		Body: body,
-	}
-
-}
-
-func (w *whatsAppMessage) GetBody() string {
-	return w.Body
-}
-
-func NormalizeWhatsAppMessage(message *WhatsAppJSONReceived) *Chat {
-	return &Chat{
-		Id:        message.Id,
-		MessageId: message.Metadata.MessageId,
-		Message:   NewWhatsAppMessage(message.Metadata.Body),
-		Delivered: ChatMessageTime{
-			Status:     "sending",
-			ReceivedAt: message.Metadata.Timestamp,
-		},
-	}
-}
+//func NormalizeWhatsAppMessage(message *WhatsAppJSONReceived) *Chat {
+//	return &Chat{
+//		MessageId: message.Metadata.MessageId,
+//		Message:   NewWhatsAppMessage(message.Metadata.Body),
+//		Delivered: ChatMessageTime{
+//			Status:     "sending",
+//			ReceivedAt: message.Metadata.Timestamp,
+//		},
+//	}
+//}
