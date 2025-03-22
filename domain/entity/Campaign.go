ce08e48f9b59a49bd95af14823f987c812fb04f1
Saimon <saimonribeiros@hotmail.com>
2025-03-22 10:37:49 -0300

    feat: templates

--- domain/entity/Campaign.go
+++ domain/entity/Campaign.go
@@ -9,6 +9,7 @@ type Campaign struct {
 	Id               string                             `json:"id" bson:"_id,omitempty"`
 	Name             string                             `json:"name" bson:"name"`
 	HasIaInteraction bool                               `json:"hasIaInteraction" bson:"hasIaInteraction"`
+	IaContext        string                             `json:"iaContext" bson:"iaContext"`
 	Template         base.TemplateInfo                  `json:"template" bson:"template"`
 	ContactsId       []string                           `json:"contactsId" bson:"contactsId"`
 	Variables        []valueobject.Pair[string, string] `json:"variables" bson:"variables"`
