02f14116e49cb4cd662220ecc25ce9d7af430232
Saimon <saimonribeiros@hotmail.com>
2025-04-06 22:21:57 -0300

    feat: templates

--- domain/entity/Campaign.go
+++ domain/entity/Campaign.go
@@ -6,12 +6,18 @@ import (
 )
 
 type Campaign struct {
-	Id               string                             `json:"id" bson:"_id,omitempty"`
-	Name             string                             `json:"name" bson:"name"`
-	HasIaInteraction bool                               `json:"hasIaInteraction" bson:"hasIaInteraction"`
-	IaContext        string                             `json:"iaContext" bson:"iaContext"`
-	Template         base.TemplateInfo                  `json:"template" bson:"template"`
-	ContactsId       []string                           `json:"contactsId" bson:"contactsId"`
-	Variables        []valueobject.Pair[string, string] `json:"variables" bson:"variables"`
-	base.Entity      `bson:",inline"`
+	Id          string                             `json:"id" bson:"_id,omitempty"`
+	Name        string                             `json:"name" bson:"name"`
+	Flows       map[string]*Flow                   `json:"flows" bson:"flows"`
+	Template    base.TemplateInfo                  `json:"template" bson:"template"`
+	ContactsId  []string                           `json:"contactsId" bson:"contactsId"`
+	Variables   []valueobject.Pair[string, string] `json:"variables" bson:"variables"`
+	base.Entity `bson:",inline"`
+}
+
+type Flow struct {
+	Id               string `json:"id" bson:"_id,omitempty"`
+	Role             string `json:"role" bson:"role,omitempty"`
+	Message          string `json:"message" bson:"message"`
+	HasIaInteraction bool   `json:"hasIaInteraction" bson:"hasIaInteraction"`
 }
