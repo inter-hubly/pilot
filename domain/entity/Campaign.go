91769cce5a846699d38b6597c4a0f0e3ff6872b0
Saimon <saimonribeiros@hotmail.com>
2025-04-22 09:17:26 -0300

    feat: templates

--- domain/entity/Campaign.go
+++ domain/entity/Campaign.go
@@ -11,13 +11,14 @@ type Campaign struct {
 	Flows       map[string]*Flow                   `json:"flows" bson:"flows"`
 	Template    base.TemplateInfo                  `json:"template" bson:"template"`
 	ContactsId  []string                           `json:"contactsId" bson:"contactsId"`
+	IaContext   string                             `json:"iaContext" bson:"iaContext"`
 	Variables   []valueobject.Pair[string, string] `json:"variables" bson:"variables"`
 	base.Entity `bson:",inline"`
 }
 
 type Flow struct {
-	Id               string `json:"id" bson:"_id,omitempty"`
-	Role             string `json:"role" bson:"role,omitempty"`
-	Message          string `json:"message" bson:"message"`
-	HasIaInteraction bool   `json:"hasIaInteraction" bson:"hasIaInteraction"`
+	Id              string `json:"id" bson:"_id,omitempty"`
+	Role            string `json:"role" bson:"role,omitempty"`
+	Message         string `json:"message" bson:"message"`
+	IsIaInteraction bool   `json:"isIaInteraction" bson:"isIaInteraction"`
 }
